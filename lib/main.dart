e071d16a579d008eb6d7be5142de8bbd6f18b241
abhinavcool42 <abhinavcool42@gmail.com>
2023-06-18 19:40:55 +0530

    changed imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/screens/homeScreen.dart';
-import 'package:todo_app/screens/LoginScreen.dart';
+import 'package:todo_app/screens/home.dart';
+import 'package:todo_app/screens/login.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
